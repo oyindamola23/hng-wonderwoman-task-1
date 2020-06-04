e6bcd15dc8959a93eb891dee44ba37dc2005526f
oyindamola23 <49127453+oyindamola23@users.noreply.github.com>
2020-06-04 14:35:30 -0700

    Revert "Create 00825-Oyindamola.js"

--- scripts/00825-Oyindamola.js
+++ scripts/00825-Oyindamola.js
@@ -1,9 +1,8 @@
-let name = 'Kareem Taiwo';
+let name = 'Kareem Taiwo Oyindamola';
 let email = 'oyindamolataiwo23@gmail.com';
 let id = 'HNG-00825';
 let lang = 'Javascript';
-
-function myDetails() {
-return ('Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}');
+funtion myDetails(){
+return ('Hello World,this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task ${email}');
 }
 console.log(myDetails());
